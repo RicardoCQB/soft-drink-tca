eac23e7a07db03e8ec568a2b96c46ab8c492e39f
PedroSerran0 <up201605633@fe.up.pt>
2020-12-04 17:54:00 +0000

    [Mechanics] player can now rotate according to mouse position

--- Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/PlayerMovement.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -8,19 +8,30 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed=5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
+   
     
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        rb.rotation = angle;
 
     }
 }
