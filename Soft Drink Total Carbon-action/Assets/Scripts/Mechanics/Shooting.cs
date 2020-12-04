336371ab2f468a39afd437aafe93957517c90160
PedroSerran0 <up201605633@fe.up.pt>
2020-12-04 18:53:34 +0000

    [Mechanics] Player can now shoot with a mouse click; added the bullet placeholder sprite (no bullet interactions yet)

---
+++ Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 10f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        //instantiates the bullet prefab according to the fire point position/rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position,
+                                                            firePoint.rotation);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+
+        
+    }
+}
