6ff148c99b60472f6351d41bc8ada529107aba95
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-12-04 10:51:52 +0000

    Dialog System almost working

---
+++ Soft Drink Total Carbon-action/Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialog dialogue;
+
+    public void TriggerDialog()
+    {
+        // Singleton pattern?
+
+        FindObjectOfType<DialogManager>().StartDialog(dialogue);
+    }
+}
