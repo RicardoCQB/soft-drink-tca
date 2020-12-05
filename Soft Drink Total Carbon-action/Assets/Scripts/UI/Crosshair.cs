9ba05fae66621861ca852cff669e6ce467ec2abf
Nuno Freitas <75166566+up202003057@users.noreply.github.com>
2020-12-05 17:54:03 +0000

    HUD - Adds crosshair and text manipulation script + Layout adjustments
    The script allows for the text to be changed by the game mechanics scripts. Currently, I'm using test int variables but this should already have some of the bases for integration with the game mechanics.

--- Soft Drink Total Carbon-action/Assets/Scripts/UI/Crosshair.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/UI/Crosshair.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    [SerializeField] Camera cam;
-    private void Update()
+    [SerializeField] Texture2D texture;
+    Vector2 cursorTxtOffset;
+    private void Start()
     {
-        transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+        // By default, the pivot of a cursor texture is the upper left corner.
+        // This changes the pivot to the center, for better precision:
+        cursorTxtOffset = new Vector2(texture.width / 2, texture.height / 2);
+
+        // Changes the cursor texture to the one we want:
+        Cursor.SetCursor(texture, cursorTxtOffset, CursorMode.Auto);
     }
 }
