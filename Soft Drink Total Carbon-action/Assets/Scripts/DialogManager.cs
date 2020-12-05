47f3403fa46698de9e21035e65aae3562e8a5215
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-12-05 02:10:24 +0000

    Revert "DialogSystem works with small sentences"
    This reverts commit c9de448f24695a4b00101826d6d6f499bec74143.

--- Soft Drink Total Carbon-action/Assets/Scripts/DialogManager.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/DialogManager.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+
 public class DialogManager : MonoBehaviour
 {
     private Queue<string> sentences;
-    public TextMeshProUGUI speakerName;
-    public TextMeshProUGUI dialogText;
 
     private void Start()
     {
@@ -16,35 +13,6 @@ public class DialogManager : MonoBehaviour
 
     public void StartDialog(Dialog dialogue)
     {
-        speakerName.text = dialogue.speakerName;
         Debug.Log("Starting conversation with: " + dialogue.speakerName);
-
-        sentences.Clear();
-
-        foreach (string sentence in dialogue.sentences)
-        {
-            sentences.Enqueue(sentence);
-        }
-
-        DisplayNextSentence();
-    }
-
-    public void DisplayNextSentence()
-    {
-        if(sentences.Count == 0)
-        {
-            EndDialogue();
-            return;
-        }
-
-        string sentence = sentences.Dequeue();
-
-        dialogText.text = sentence;
-        Debug.Log(sentence);
-    }
-
-    public void EndDialogue()
-    {
-        Debug.Log("End of conversation!");
     }
 }
