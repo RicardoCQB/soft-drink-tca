7c024ea58281a4976221eb355916c9507d30b746
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-12-08 15:36:52 +0000

    Changed gun sprite movement

--- Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/PlayerMovement.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -19,19 +19,18 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
 
         Vector2 lookDir = mousePos - rb.position;
 
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-
-        rb.rotation = angle;
-
-    }
+    }*/
 }
