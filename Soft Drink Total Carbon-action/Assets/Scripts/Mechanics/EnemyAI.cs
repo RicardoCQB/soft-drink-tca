1b2b4f6af66e6b58c8f0e0054d26d5e8c51da59a
PedroSerran0 <up201605633@fe.up.pt>
2020-12-11 17:26:52 +0000

    [Mechanics] Added Enemy AI placeholder object and bullet; added AI movement and shooting script and enemy bullet script

---
+++ Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBetweenShots;
+    public float startTimeBetweenShots;
+
+    public Transform firePoint;
+    public GameObject projectile;
+    public Transform player;
+
+    private void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBetweenShots = startTimeBetweenShots;
+    }
+
+    private void Update()
+    {
+        //ENEMY MOVEMENT
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position,
+                                                               speed * Time.deltaTime);
+
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance
+                    && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position,
+                                                               -speed * Time.deltaTime);
+        }
+
+        //ENEMY SHOOTING
+        if (timeBetweenShots <= 0)
+        {
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
+            timeBetweenShots = startTimeBetweenShots;
+        }
+        else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+
+    }
+}
---
+++ Soft Drink Total Carbon-action/Assets/Scripts/Mechanics/EnemyBullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed;
+
+
+    private Transform player;
+    private Vector2 target;
+    private Vector2 overshoot; //if we want it to keep going after it would stop
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+
+        //makes the projectile keep going instead of stopping at its destination (thats how "Vector2.MoveTowards() works)
+        overshoot = (player.position - transform.position).normalized * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+
+        Vector3 overshotPos = new Vector3(target.x + overshoot.x, target.y + overshoot.y, 1);
+
+        //shoot bullet towards player
+        transform.position = Vector2.MoveTowards(transform.position, overshotPos , speed * Time.deltaTime);
+
+        Vector2 transformPosition = new Vector2(transform.position.x, transform.position.y);
+
+        //if the projectile stops (if its overshooting, this probably wont be necessary)
+        if (currentPosition == transformPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+}
