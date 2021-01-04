74528ff7b1c716273db5b88c31747aaa36cf9c3e
Nuno Freitas <75166566+up202003057@users.noreply.github.com>
2021-01-04 21:32:42 +0000

    Store UI - More canvas work / Menu UI - Now loads level

--- Soft Drink Total Carbon-action/Assets/Scripts/UI/UserInterface.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/UI/UserInterface.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Library added specifically for this script.
 
 public class UserInterface : MonoBehaviour
 {
@@ -13,5 +14,15 @@ public class UserInterface : MonoBehaviour
     public void DisableGameObject(GameObject gameObject)
     {
         gameObject.SetActive(false);
+    }
+    public void LoadScene(string sceneToLoad)
+    {
+        // This will replace the active scene with the one you want to load.
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+    public void ExitApplication()
+    {
+        // This only works on exported builds, not in the editor's Play Mode.
+        Application.Quit();
     }
 }
