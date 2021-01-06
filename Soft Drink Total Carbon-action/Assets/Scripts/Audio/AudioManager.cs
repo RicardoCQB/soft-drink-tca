8b1d9cb39635b9fd5e052e423a166bb3925c52a3
RicardoCQB <ricardo_cqb@hotmail.com>
2021-01-06 21:54:17 +0000

    hope this works 👨‍🚒

---
+++ Soft Drink Total Carbon-action/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    //starts before Start()
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
