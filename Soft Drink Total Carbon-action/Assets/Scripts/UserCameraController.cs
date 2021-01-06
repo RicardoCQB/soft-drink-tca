e47057abebb4525ce532dc4232b65bf251b4411c
RicardoCQB <ricardo_cqb@hotmail.com>
2021-01-06 21:39:42 +0000

    created new tile palettes

--- Soft Drink Total Carbon-action/Assets/Scripts/UserCameraController.cs
+++ Soft Drink Total Carbon-action/Assets/Scripts/UserCameraController.cs
@@ -14,10 +14,13 @@ public class UserCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //TODO: Try out if statement that calculates distance from player to mouse and decides if the camera is moved a bit to the mouse transform.
+
         if (Player != null)
         {
             mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            cameraPos = new Vector3((Player.position.x + mousePos.x)/2, (Player.position.y + mousePos.y)/2, -10f);
+            //cameraPos = new Vector3((Player.position.x + mousePos.x)/2, (Player.position.y + mousePos.y)/2, -10f);
+            cameraPos = new Vector3(Player.position.x, Player.position.y, -10f);
         }
         transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
     }
